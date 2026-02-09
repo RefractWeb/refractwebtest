7c263e4adda55e335eacfd5a087b697efc036486
Anish Biswas <xevenbiswas@gmail.com>
2026-02-10 02:18:37 +0530

    feat: wrap buttons in ActionButtons component with Link for improved navigation

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -1,17 +1,22 @@
 import { Button } from "./ui/button";
 import { ArrowRight } from "lucide-react";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const ActionButtons = ({ className }: { className?: string }) => {
   return (
     <div
       className={cn("flex flex-col sm:flex-row items-center gap-6", className)}
     >
-      <Button size={"lg"}>Work With Us</Button>
-      <Button variant={"ghost"} className="group ">
-        Explore our services
-        <ArrowRight className="size-4 group-hover:translate-x-1 transition-transform" />
-      </Button>
+      <Link href="/contact">
+        <Button size={"lg"}>Work With Us</Button>
+      </Link>
+      <Link href="/#services">
+        <Button variant={"ghost"} className="group ">
+          Explore our services
+          <ArrowRight className="size-4 group-hover:translate-x-1 transition-transform" />
+        </Button>
+      </Link>
     </div>
   );
 };
