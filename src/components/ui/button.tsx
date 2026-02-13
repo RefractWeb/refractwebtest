791ba912d56d39893f86d5885dbdbb5f4d101ce6
Anish Biswas <xevenbiswas@gmail.com>
2026-02-13 17:16:55 +0530

    refactor: adjust spacing in BentoSection and button component for improved layout

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,10 +21,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        default: "h-9 px-3 md:px-4 py-2 has-[>svg]:px-3",
         xs: "h-6 gap-1 px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
         sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "px-6 py-2 has-[>svg]:px-4 ",
+        lg: "md:px-6 px-3 sm:px-4 py-2 has-[>svg]:px-4 ",
         icon: "size-9",
         "icon-xs": "size-6 [&_svg:not([class*='size-'])]:size-3",
         "icon-sm": "size-8",
