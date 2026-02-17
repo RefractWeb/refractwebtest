7cd4eddb968aec35f97e89050a403b27502ac344
Anish Biswas <xevenbiswas@gmail.com>
2026-02-18 03:05:48 +0530

    feat: Refine CTASection layout and enhance visual effects

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -9,12 +9,12 @@ export const CTASection = () => {
   return (
     <section className="pt-16 md:py-24 md:mb-10 px-6 flex justify-center relative">
       <div className="container max-w-6xl relative z-10 flex flex-col lg:flex-row items-center justify-between gap-16">
-        <div className="w-full lg:w-1/2 space-y-5 md:space-y-8">
+        <div className="w-full md:w-1/2 space-y-5 md:space-y-8">
           <AnimatedText
             useScrollTrigger={true}
             animationType="slideUp"
             splitType="lines"
-            className="text-5xl lg:text-6xl font-bold tracking-tight leading-[1.2]"
+            className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight leading-[1.2]"
           >
             Ready for <br />
             what's next?
@@ -24,23 +24,32 @@ export const CTASection = () => {
             animationType="slideUp"
             splitType="lines"
             delay={0.1}
-            className="text-muted-foreground text-xl max-w-xs"
+            className="text-muted-foreground text-lg md:text-xl max-w-xs"
           >
             Let's discuss your vision and see if we are the right fit.
           </AnimatedText>
           <ActionButtons />
         </div>
 
-        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end relative isolate">
-          <div className="absolute inset-0 hidden md:block">
-            <div className="absolute h-2 rounded-full bg-linear-to-r to-white via-white rotate-25 z-10 w-300 mix-blend-soft-light blur-[2px] -left-211 transform-gpu-blur"></div>
-            <div className="absolute w-300 h-100 rotate-25 -top-80 -right-4 overflow-hidden">
-              <div className="absolute size-100 rounded-full bg-primary2 z-10 -bottom-80 left-1/2 -translate-x-1/2 blur-[130px] blur-min"></div>
-            </div>
+        <div className="w-full h-full md:w-1/2 flex justify-center md:justify-end relative isolate">
+          <div className="hidden md:block absolute inset-0 z-10 pointer-events-none">
+            <div
+              className="absolute inset-0 origin-center translate-x-60 -translate-y-10"
+              style={{ transform: "rotate(25deg)" }}
+            >
+              {/* Top light streak */}
+              <div className="absolute h-[0.5%] rounded-full bg-linear-to-r to-white via-white z-10 w-[200%] mix-blend-soft-light blur-[2px] right-[90%] top-[80%] transform-gpu-blur"></div>
+              {/* Top glow */}
+              <div className="absolute w-[200%] h-full -top-[20%] right-[75%] overflow-hidden">
+                <div className="absolute w-[42%] aspect-square rounded-full bg-primary2 z-10 -bottom-[70%] left-1/2 -translate-x-1/2 blur-[120px] blur-min"></div>
+              </div>
 
-            <div className="absolute h-2 rounded-full bg-linear-to-l to-white via-white rotate-25 top-99 z-10 w-300 mix-blend-soft-light blur-[2px] transform-gpu-blur"></div>
-            <div className="absolute w-250 h-100 rotate-25 top-94 left-0 overflow-hidden">
-              <div className="absolute size-100 rounded-full bg-[#4661BE] z-10 -top-90 left-1/2 -translate-x-1/2 blur-[130px] blur-min"></div>
+              {/* Bottom light streak */}
+              <div className="absolute h-[0.5%] rounded-full bg-linear-to-l to-white via-white z-10 w-[200%] mix-blend-soft-light blur-[2px] top-[80%] -left-[10%] transform-gpu-blur"></div>
+              {/* Bottom glow */}
+              <div className="absolute w-[150%] h-[70%] top-[80%] left-[10%] overflow-hidden">
+                <div className="absolute w-[42%] aspect-square rounded-full bg-[#4661BE] z-10 -top-[80%] left-1/2 -translate-x-1/2 blur-[120px] blur-min"></div>
+              </div>
             </div>
           </div>
           <AnimatedChip className="z-20 scale-120" />
